cfef002e0f57255f2eea9e59633d9981ee9a8d21
Varshini Ananta <sananta@ucdavis.edu>
2019-05-26 23:57:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,7 +87,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if !won?(board) && full?(board)
+  if full?(board) && !won?(board)
     return true;
   elsif !won?(board) && !full?(board)
     return false;
