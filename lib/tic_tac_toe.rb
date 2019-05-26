2bf41da2d84d34df03cbfb639c750b70c4c55be9
Varshini Ananta <sananta@ucdavis.edu>
2019-05-26 23:49:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,3 +39,15 @@ def valid_move?(board, index)
     return true
   end
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board);
+  end
+end
