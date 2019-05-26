41f0c0ac565e7ee146cfdda16424f8a5a0f132a3
Varshini Ananta <sananta@ucdavis.edu>
2019-05-26 23:47:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,3 +25,17 @@ end
 def move(array, index, ch = "X")
   array[index] = ch
 end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def valid_move?(board, index)
+  if index < 0 || index > 8
+    return false
+  elsif position_taken?(board, index)
+    return false
+  else
+    return true
+  end
+end
